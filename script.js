7c32a245134d8d2b9a0c3c7050ea253fdda940ea
dartrix501 <luinav2007@gmail.com>
2025-10-14 20:09:46 -0500

    Implementación de la Api

--- script.js
+++ script.js
@@ -56,4 +56,34 @@ document.addEventListener('DOMContentLoaded', () => {
     images.forEach(img => observercurso.observe(img));
 
 
+    // Formulario
+
+    const form = document.getElementById("contactForm");
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const nombre = document.getElementById("nombre").value;
+        const email = document.getElementById("email").value;
+        const numero = document.getElementById("numero").value;
+
+        if (!nombre || !email || !numero) {
+            alert("Por favor, complete todos los campos.");
+            return;
+        }
+
+        try {
+            const res = await fetch("https://api-test-2hpy.onrender.com/procesar", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ nombre, email })
+            });
+
+            const data = await res.json();
+            alert("¡Datos enviados correctamente!");
+        } catch (err) {
+            console.error("Error:", err);
+            alert("Ocurrió un error al enviar los datos");
+        }
+    });
 });
